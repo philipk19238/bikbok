5bafebde7d5df370be77c89c91c86fff33852de9
andrew-arkhipov <andrew-arkhipov@users.noreply.github.com>
2021-04-10 16:45:19 -0500

    added similarity

--- app/routes/analyze/views.py
+++ app/routes/analyze/views.py
@@ -7,12 +7,17 @@ from ...pipelines import *
 analyze_bp = Blueprint(
     'analyze_bp', __name__, template_folder='templates', url_prefix='/analyze')
 
+top_songs = get_top_songs()
+genre_vectors = generate_genre_vectors()
+
 class AnalyzeView(MethodView):
 
     template = 'analyze.html'
 
     def post(self):
-        song_name = request.form['song_name']
-        return render_template(self.template, song=song_name)
+        song_id = request.form['song_id']
+        song = generate_vector()
+        similarity = genre_similarity(song, top_songs)
+        return render_template(self.template, song=song_id)
 
 analyze_bp.add_url_rule('/', view_func=AnalyzeView.as_view('analyze'))
