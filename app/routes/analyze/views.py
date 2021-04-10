850b62163d214976167e9b04de1e0391e176c739
andrew-arkhipov <andrew-arkhipov@users.noreply.github.com>
2021-04-10 16:51:24 -0500

    made fixes because of rushing lol

--- app/routes/analyze/views.py
+++ app/routes/analyze/views.py
@@ -7,8 +7,8 @@ from ...pipelines import *
 analyze_bp = Blueprint(
     'analyze_bp', __name__, template_folder='templates', url_prefix='/analyze')
 
-top_songs = get_top_songs()
-genre_vectors = generate_genre_vectors()
+top_song_ids = get_top_songs()
+genre_vectors = generate_genre_vectors(top_song_ids)
 
 class AnalyzeView(MethodView):
 
@@ -16,8 +16,8 @@ class AnalyzeView(MethodView):
 
     def post(self):
         song_id = request.form['song_id']
-        song = generate_vector()
-        similarity = genre_similarity(song, top_songs)
-        return render_template(self.template, song=song_id)
+        song = generate_vector(song_id)
+        similarity = genre_similarity(song, top_song_ids)
+        return render_template(self.template, song=song_id, similarity=similarity)
 
 analyze_bp.add_url_rule('/', view_func=AnalyzeView.as_view('analyze'))
