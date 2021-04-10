49e57a5573dd2e067634121863c8a102b0d3c9de
andrew-arkhipov <andrew-arkhipov@users.noreply.github.com>
2021-04-10 13:59:13 -0500

    first commit for spotify

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 import os 
 
 from flask import Flask
+from .config import config
 
 def register_blueprints(app):
     pass
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_object(config)
+
     register_blueprints(app)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
+config = Config()
